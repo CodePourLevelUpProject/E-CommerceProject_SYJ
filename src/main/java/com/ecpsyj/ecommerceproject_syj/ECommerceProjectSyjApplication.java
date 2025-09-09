289613e80665f3d7a21e901569a8fcdf1ce68964
OZIIJIN <dpwlsdlqma6@gmail.com>
2025-09-10 00:03:25 +0900

    init

---
+++ src/main/java/com/ecpsyj/ecommerceproject_syj/ECommerceProjectSyjApplication.java
@@ -0,0 +1,13 @@
+package com.ecpsyj.ecommerceproject_syj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProjectSyjApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ECommerceProjectSyjApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ecpsyj/ecommerceproject_syj/ECommerceProjectSyjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecpsyj.ecommerceproject_syj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceProjectSyjApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
